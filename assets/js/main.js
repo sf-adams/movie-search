195847d861c7f109d7dc99212ce2051eea99c8c2
Sam <samuel.fitz@btinternet.com>
2022-02-09 12:05:42 +0000

    Modal and Services Sections Added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,38 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+function scrollHeader(){
+  const header = document.getElementById('header')
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SERVICES MODAL ===============*/
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalClose = document.querySelectorAll('.services__modal-close')
 
+let modal = function(modalClick){
+  modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((mb, i) => {
+  mb.addEventListener('click', () => {
+    modal(i)
+  })
+})
+
+modalClose.foreEach((mc) => {
+  mc.addEventListener('click', () => {
+    modalViews.forEach((mv) => {
+      mv.classList.remove('active-modal')
+    })
+  })
+})
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
 
-/* Link active work */ 
+/* Link active work */
 
 
 /*=============== SWIPER TESTIMONIAL ===============*/
@@ -16,7 +41,7 @@
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
-/*=============== LIGHT DARK THEME ===============*/ 
+/*=============== LIGHT DARK THEME ===============*/
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
